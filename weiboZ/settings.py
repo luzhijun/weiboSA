b3d9bc6a2fee6bc8bcc3feb35559007e242e81ca
trucy <luzj@vparam.com>
2016-10-30 23:09:10 +0800

    增加随机agent，可选代理ip

--- weiboZ/settings.py
+++ weiboZ/settings.py
@@ -9,17 +9,38 @@
 #     http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
 #     http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
 
+
+USER_AGENTS = [
+    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; AcooBrowser; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
+    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; Acoo Browser; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.0.04506)",
+    "Mozilla/4.0 (compatible; MSIE 7.0; AOL 9.5; AOLBuild 4337.35; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
+    "Mozilla/5.0 (Windows; U; MSIE 9.0; Windows NT 9.0; en-US)",
+    "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 2.0.50727; Media Center PC 6.0)",
+    "Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 1.0.3705; .NET CLR 1.1.4322)",
+    "Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 5.2; .NET CLR 1.1.4322; .NET CLR 2.0.50727; InfoPath.2; .NET CLR 3.0.04506.30)",
+    "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN) AppleWebKit/523.15 (KHTML, like Gecko, Safari/419.3) Arora/0.3 (Change: 287 c9dfb30)",
+    "Mozilla/5.0 (X11; U; Linux; en-US) AppleWebKit/527+ (KHTML, like Gecko, Safari/419.3) Arora/0.6",
+    "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.2pre) Gecko/20070215 K-Ninja/2.1.1",
+    "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9) Gecko/20080705 Firefox/3.0 Kapiko/3.0",
+    "Mozilla/5.0 (X11; Linux i686; U;) Gecko/20070322 Kazehakase/0.4.5",
+    "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.8) Gecko Fedora/1.9.0.8-1.fc10 Kazehakase/0.5.6",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.20 (KHTML, like Gecko) Chrome/19.0.1036.7 Safari/535.20",
+    "Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; fr) Presto/2.9.168 Version/11.52",
+]
+# 代理地址，可以去http://www.ip181.com/找，及时更新验证
+#PROXIES = ["http://183.131.76.27:8888", "http://121.40.108.76:80", "http://123.160.143.111:8888", "http://111.201.239.27:81", "http://182.203.83.205:8888", "http://183.129.151.130:80", "http://123.123.19.190:81", "http://202.171.253.72:80", "http://122.72.32.74:80", "http://182.88.207.145:8123", "http://120.92.3.165:80", "http://106.75.176.4:80", "http://122.96.59.107:843", "http://218.3.177.36:8089", "http://182.37.22.192:8118", "http://140.255.17.169:8888", "http://122.5.224.189:8888", "http://115.236.75.201:80", "http://139.196.108.68:80", "http://113.3.74.103:8118", "http://122.72.32.88:80", "http://121.225.217.204:8888", "http://27.219.26.68:8888", "http://121.14.6.236:80", "http://119.28.19.222:8888", "http://42.96.129.41:8088", "http://117.28.255.84:80",]
 BOT_NAME = 'weiboZ'
 
 SPIDER_MODULES = ['weiboZ.spiders']
 NEWSPIDER_MODULE = 'weiboZ.spiders'
 
-LOG_LEVEL = 'WARNING'
+LOG_LEVEL = 'INFO'
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'weiboZ (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 MONGO_URI = 'mongodb://localhost:27017/'
 MONGO_DATABASE = {
@@ -48,7 +69,7 @@ COOKIES_ENABLED = False
 DEFAULT_REQUEST_HEADERS = {
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
     'Accept-Language': 'zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3',
-    'User-Agent:': 'Mozilla/5.0 (iPhone; CPU iPhone OS 5_1_1 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9B206 Safari/7534.48.3'
+    'User-Agent:': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.20 (KHTML, like Gecko) Chrome/19.0.1036.7 Safari/535.20",
 }
 
 # Enable or disable spider middlewares
@@ -59,9 +80,11 @@ DEFAULT_REQUEST_HEADERS = {
 
 # Enable or disable downloader middlewares
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
-# DOWNLOADER_MIDDLEWARES = {
-#    'weiboZ.middlewares.MyCustomDownloaderMiddleware': 543,
-#}
+DOWNLOADER_MIDDLEWARES = {
+    'weiboZ.middlewares.DBDownloaderMiddleware.RandomUserAgent': 543,
+    'scrapy.downloadermiddleware.useragent.UserAgentMiddleware': None,
+    #'weiboZ.middlewares.DBDownloaderMiddleware.ProxyMiddleware': 544
+}
 
 # Enable or disable extensions
 # See http://scrapy.readthedocs.org/en/latest/topics/extensions.html
@@ -72,7 +95,9 @@ DEFAULT_REQUEST_HEADERS = {
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-    'weiboZ.pipelines.MongoPipeline': 300,
+    #'weiboZ.pipelines.weiboMongoPipeline': 300,
+    #'weiboZ.pipelines.JsonPipeline': 300,
+    'weiboZ.pipelines.dbMongoPipeline': 300
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
