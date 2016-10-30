dc899da431e105d6eda91e6460bf397db188755f
trucy <luzj@vparam.com>
2016-10-30 23:07:10 +0800

    增加豆瓣日期处理

--- weiboZ/DateUtil.py
+++ weiboZ/DateUtil.py
@@ -6,6 +6,7 @@ Created on Wed Oct 26 18:01:55 2016
 @author: Trucy
 """
 import datetime
+import hashlib
 
 def convert_in_an_hour(fr):
     now=datetime.datetime.now()
@@ -21,7 +22,8 @@ def convert_in_an_hour(fr):
 def convert(fr):
     '''
     微博时间形式:
-        wblog日期形式:[10,20,30,40,50]秒前,[1-59]分钟前，今天hh:mm，MM月DD日 hh:mm,yyyy-MM-dd hh:mm
+        wblog日期形式:[10,20,30,40,50]秒前,[1-59]分钟前，今天hh:mm，MM月DD日 hh:mm, yyyy-MM-dd hh:mm
+    豆瓣时间格式: yyyy-MM-dd hh:mm:ss
     返回USC时间
     '''
     delta=datetime.timedelta(hours=8)
@@ -36,12 +38,18 @@ def convert(fr):
         mon=fr[1] if fr[0]=='0' else fr[:2]
         day=fr[4] if fr[3]=='0' else fr[3:5] 
         return datetime.datetime(td.year,int(mon),int(day),int(hour),int(minute))-delta
-    else:
+    if len(fr)==16:
         dt=fr.split('-')
         if len(dt[1])==1:
             dt[1]='0'+dt[1]
         if len(dt[2])==1:
             dt[2]='0'+dt[2]
         fr=''.join(dt)
-        return datetime.datetime.strptime(fr,"%Y-%m-%d %H:%M")-delta
-                            
+        return datetime.datetime.strptime(fr,"%Y%m%d %H:%M")-delta
+    else: ##豆瓣时间格式
+        return datetime.datetime.strptime(fr,"%Y-%m-%d %H:%M:%S")-delta
+
+def calc_md5(s):
+    md5 = hashlib.md5()
+    md5.update(s.encode('utf-8'))
+    return md5.hexdigest()
